8273debd797b2f1641282e80d45e39728962bf0f
CleversonM2007 <cleversonmarceloacelino@gmail.com>
2025-08-20 01:37:16 -0300

    FirstProject CSharp
    Fazendo alguns testes basicos usando o console.writeline e console.write

---
+++ FirstProject/Program.cs
@@ -0,0 +1,14 @@
+﻿//Testes
+
+//teste1
+Console.WriteLine("Hello World");
+Console.WriteLine("Hello World!");
+Console.WriteLine(" ");
+//teste2
+Console.Write("Hello World");
+Console.WriteLine("Hello World!");
+Console.WriteLine(" ");
+//teste3
+Console.Write("Hello World");
+Console.Write("Hello World!");
+
